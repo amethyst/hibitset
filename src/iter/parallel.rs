6e3bd466e1e405fa74d5e4d08ccf178462b96dda
delma <delma@del.ma>
2017-11-13 00:28:48 +0200

    Initial work for improving parallel iterator splitting

--- src/iter/parallel.rs
+++ src/iter/parallel.rs
@@ -1,9 +1,8 @@
-use std::mem::size_of;
-
 use rayon::iter::ParallelIterator;
 use rayon::iter::internal::{UnindexedProducer, UnindexedConsumer, Folder, bridge_unindexed};
 
 use iter::{BITS, BitSetLike, BitIter, Index};
+use util::average_bit;
 
 /// A `ParallelIterator` over a [`BitSetLike`] structure.
 ///
@@ -44,6 +43,12 @@ impl<'a, T: 'a + Send + Sync> UnindexedProducer for BitProducer<'a, T>
     where T: BitSetLike
 {
     type Item = Index;
+    /// The splitting strategy used assumes that the bitset is distributed
+    /// axproximately uniformly.
+    ///
+    /// Splitting the bitset into two parts is done highest level
+    /// that still has more than one children.
+    /// TODO: Better explanation of the algorithm.
     fn split(mut self) -> (Self, Option<Self>) {
         let other = {
             let mut handle_level = |level: usize| {
@@ -51,49 +56,46 @@ impl<'a, T: 'a + Send + Sync> UnindexedProducer for BitProducer<'a, T>
                 if self.0.masks[level] == 0 {
                     return None;
                 }
-                // Find first bit that is set
-                let first_bit = self.0.masks[level].trailing_zeros();
-                // Find last bit that is set
-                let usize_bits = size_of::<usize>() * 8;
-                let last_bit = usize_bits as u32 - self.0.masks[level].leading_zeros() - 1;
+                let average_bit = average_bit(self.0.masks[level]);
+
                 // If this is the highest level, there is no prefix saved as it's always zero
                 let level_prefix = self.0.prefix.get(level).cloned().unwrap_or(0);
                 // If there is one bit left, descend
-                if first_bit == last_bit {
+                let first_bit = self.0.masks[level].trailing_zeros();
+
+                if let Some(average_bit) = average_bit {
+                    let mask = (1 << (average_bit - 1)) - 1;
+
+                    let mut other = BitProducer(BitIter::new(self.0.set, [0, 0, 0, 0], [0, 0, 0]));
+                    let original_mask = self.0.masks[level];
+                    // Take the higher half of the mask
+                    other.0.masks[level] = original_mask & !mask;
+                    // The higher levels of masks don't need to preserved as they are empty.
+                    for n in &self.0.masks[(level + 1)..] {
+                        debug_assert_eq!(*n, 0);
+                    }
+                    // The higher half starts iterating after the average
+                    other.0.prefix[level - 1] = (level_prefix | average_bit as u32) << BITS;
+                    // And preserve the prefix of the higher levels
+                    other.0.prefix[level..].copy_from_slice(&self.0.prefix[level..]);
+                    // Take the lower half the mask
+                    self.0.masks[level] = original_mask & mask;
+                    // Combined mask of the current level should now equal the original mask
+                    debug_assert_eq!(self.0.masks[level] | other.0.masks[level], original_mask);
+                    // The lower half starts iterating from the first bit
+                    self.0.prefix[level - 1] = (level_prefix | first_bit) << BITS;
+                    Some(other)
+                } else {
                     // Calculate the index based on prefix and the bit that is descended to
-                    let idx = (level_prefix | first_bit) as usize;
+                    let idx = level_prefix as usize | first_bit as usize;
                     // When descending all of the iteration happens in the child of the bit that is left
                     self.0.prefix[level - 1] = (idx as u32) << BITS;
                     // And because all the iteration happens in the child, it's parent can be removed.
                     self.0.masks[level] = 0;
                     // Get the mask of the child layer from the set
                     self.0.masks[level - 1] = get_from_layer(self.0.set, level - 1, idx);
-                    return None;
+                    None
                 }
-                // Make the split point to be the average of first and last bit.
-                let after_average = (first_bit + last_bit) / 2 + 1;
-                // A bit mask to get the lower half of the mask
-                // This cuts the mask to half from the average
-                let mask = (1 << after_average) - 1;
-                let mut other = BitProducer(BitIter::new(self.0.set, [0, 0, 0, 0], [0, 0, 0]));
-                let original_mask = self.0.masks[level];
-                // Take the higher half of the mask
-                other.0.masks[level] = original_mask & !mask;
-                // The higher levels of masks don't need to preserved as they are empty.
-                for n in &self.0.masks[(level + 1)..] {
-                    debug_assert_eq!(*n, 0);
-                }
-                // The higher half starts iterating after the average
-                other.0.prefix[level - 1] = (level_prefix | after_average) << BITS;
-                // And preserve the prefix of the higher levels
-                other.0.prefix[level..].copy_from_slice(&self.0.prefix[level..]);
-                // Take the lower half the mask
-                self.0.masks[level] = original_mask & mask;
-                // Combined mask of the current level should now equal the original mask
-                debug_assert_eq!(self.0.masks[level] | other.0.masks[level], original_mask);
-                // The lower half starts iterating from the first bit
-                self.0.prefix[level - 1] = (level_prefix | first_bit) << BITS;
-                Some(other)
             };
             handle_level(3)
                 .or_else(|| handle_level(2))
@@ -107,6 +109,44 @@ impl<'a, T: 'a + Send + Sync> UnindexedProducer for BitProducer<'a, T>
     {
         folder.consume_iter(self.0)
     }
+}
+
+#[test]
+fn max_splitting_of_two_top_bits() {
+    fn visit<T>(mut us: BitProducer<T>, d: usize, i: usize, mut trail: String, c: &mut usize)
+        where T: Send +
+                 Sync +
+                 BitSetLike
+    {
+        if d == 0 {
+            assert!(us.split().1.is_none());
+            *c += 1;
+        } else {
+            for j in 1..(i + 1) {
+                let (new_us, them) = us.split();
+                us = new_us;
+                let them = them.expect(&trail);
+                let mut trail = trail.clone();
+                trail.push_str(&i.to_string());
+                visit(them, d, i - j, trail, c);
+            }
+            trail.push_str("u");
+            visit(us, d - 1, 6, trail, c);
+        }
+    }
+
+    let mut c = ::BitSet::new();
+    for i in 0..524288 {
+        assert!(!c.add(i));
+    }
+
+    let us = BitProducer((&c).iter());
+    let (us, them) = us.split();
+
+    let mut count = 0;
+    visit(us, 2, 6, "u".to_owned(), &mut count);
+    visit(them.expect("Splitting top level"), 2, 6, "t".to_owned(), &mut count);
+    assert_eq!(524288 / 64, count);
 }
 
 /// Gets usize by layer and index from bit set.
